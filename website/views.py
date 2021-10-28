68fffaa2e930cbf882cf695de43948fea69b05ea
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2021-10-28 07:28:11 +0330

    add general template to website
    add index.html as a general template
    we add template folder to root of website & change location of contacts page

--- website/views.py
+++ website/views.py
@@ -13,10 +13,10 @@ from django.shortcuts import render
 #     return HttpResponse ('<h1>this is home page</h1>')
 
 def index_view(request) :
-    return render(request,"website/index.html")
+    return render(request,"index.html")
 
 def contacts_viw(request) :
-    return render(request,"contacts.html")    
+    return render(request,"website/contacts.html")    
 
 def about_view(request) :
     return render(request,"about.html")
