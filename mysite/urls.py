f088899a7d03fb9f21a0f6540a7e5195e4bffcba
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2021-11-08 21:19:42 +0330

    create knuford template

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('knuford.urls')),
     path('',include('website.urls')),
     path('',include('blog.urls'))
     
