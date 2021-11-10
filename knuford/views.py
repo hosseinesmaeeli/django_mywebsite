9ce5437e68bf94836e329b9d5c9caf31feee12fd
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2021-11-10 13:57:02 +0330

    create CV web site

--- knuford/views.py
+++ knuford/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request) :
-    return render(request,"knuford/index.html")
+    context= {
+    'name':'Hossein Esmaeeli',
+    'description_0':'I am Hossein Esmaeeli',
+    'about_me':'Im civil engineer but I like devlope my Idea and now I try to learn python and django framework with Mr.Bigdeli'
+    }
+    return render(request,"knuford/index.html",context)
