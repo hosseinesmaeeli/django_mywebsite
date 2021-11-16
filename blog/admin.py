ffce1e4a777874a8f0a4ee463b27d44a132d3dc9
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-11-16 07:54:36 +0330

    work with admin & database
    changes in model.py and admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
+
+from blog.models import Post
+class ostAdmin(admin.ModelAdmin) :
+    date_hierarchy = 'updated_date'
+    # fields = ('title', 'content')
+admin.site.register(Post, ostAdmin)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from website.models import Contact
+
+class ContactAdmin(admin.ModelAdmin) :
+    date_hierarchy = 'updated_date'
+    # fields = ('title', 'content')
+admin.site.register(Contact, ContactAdmin)
