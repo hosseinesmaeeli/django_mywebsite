74adf6a23e6c5fd48b193e5fad1e840fbd333fe9
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-11-17 08:14:52 +0330

    transfer data from database to blog page

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,8 @@ from blog.models import Post
 class ostAdmin(admin.ModelAdmin) :
     date_hierarchy = 'updated_date'
     # fields = ('title', 'content')
+    list_display = ('title' ,'counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    search_fields = ['title','content']
+
 admin.site.register(Post, ostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from blog.models import Post
 
 # Create your views here.
 def blog_index(request) :
-    context={'content':'helooooo. My name is Hossein'}
+    posts= Post.objects.filter(status=0)
+    # context={'content':'helooooo. My name is Hossein'}
+    context={'posts': posts}
     return render(request,"blog/blog-home.html",context)
 
 def blog_single(request) :
