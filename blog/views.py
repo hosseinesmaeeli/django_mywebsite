ac5ee792f2ce3675c16b3c8948cc3596bcb6667f
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-11-17 16:45:02 +0330

    url parameters & link to DB

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
@@ -11,8 +11,20 @@ def blog_index(request) :
 def blog_single(request) :
     return render(request,"blog/blog-single.html")    
 
-def test(request) :
-    posts= Post.objects.all()
-    # posts= Post.objects.filter(status=0)
-    context={'posts': posts}
-    return render(request,"blog/test.html",context)    
+# def test(request) :
+#     posts= Post.objects.all()
+#     # posts= Post.objects.filter(status=0)
+#     context={'posts': posts}
+#     return render(request,"blog/test.html",context)    
+
+
+# def test(request, name,famili_name) :
+
+#     context={'name': name, 'famili_name' : famili_name}
+#     return render(request,"blog/test.html",context)
+
+def test(request, pid) :
+    # post= Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid)
+    context={'post': post}
+    return render(request,"blog/test.html",context)
