aaed18267c579b5cb8ba42b6471e647513d15fab
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-11-20 07:24:33 +0330

    add Author field to model

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,8 @@ from blog.models import Post
 class ostAdmin(admin.ModelAdmin) :
     date_hierarchy = 'updated_date'
     # fields = ('title', 'content')
-    list_display = ('title' ,'counted_views','status','published_date','created_date')
-    list_filter = ('status',)
+    list_display = ('title' ,'author','counted_views','status','published_date','created_date')
+    list_filter = ('status','author',)
     search_fields = ['title','content']
 
 admin.site.register(Post, ostAdmin)
