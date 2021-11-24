9ad048218e2fdd2f77a90b2909ca78725e4b1f2d
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-11-24 17:11:47 +0330

    use simple tag 2

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,20 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
 
-@register.simple_tag
-def function(a):
-    return a+2
+# @register.simple_tag
+# def function(a):
+#     return a+2
+
+# @register.simple_tag(name='totalposts')   
+# def function():
+#     # posts=Post.objects.filter(status=1)
+#     posts=Post.objects.filter(status=1).count()
+#     return posts
+
+@register.simple_tag(name='totalposts')   
+def function():
+    # posts=Post.objects.filter(status=1)
+    posts=Post.objects.filter(status=1).count()
+    return posts
