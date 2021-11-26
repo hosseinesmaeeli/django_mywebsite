e34cd116733b97846371e8ad11a9ff9b9733e0f4
hosseinesmaeeli <h.esmaeeli@yahoo.com>
2021-11-26 22:05:37 +0330

    number of posts in each category in sidebar
    number of posts in each category in sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, category
+from blog.models import category
 
 register = template.Library()
 
@@ -33,3 +34,12 @@ def popularposts():
 def latestposts(arg=3):
     posts= Post.objects.filter(status=1).order_by('published_date')[:arg] #for end post
     return {'posts':posts}    
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts=Post.objects.filter(status=1)
+    categories=category.objects.all()
+    cat_dict={}
+    for name in categories :
+        cat_dict[name]= posts.filter(category=name).count()
+    return {'categories' : cat_dict}
