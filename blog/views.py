4c255ea6e90b5124ca5af1f62cd662ad117f40f4
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-11-30 14:00:41 +0330

    add page number to pages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import PageNotAnInteger, Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -20,7 +21,15 @@ def blog_index(request,**kwargs) :
     # context={'content':'helooooo. My name is Hossein'}
 
     if kwargs.get('author_username') != None:
-        posts= posts.filter(author__username = kwargs['author_username'])   
+        posts= posts.filter(author__username = kwargs['author_username'])  
+    posts= Paginator(posts,3)   
+    try:  
+        page_number= request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except PageNotAnInteger :
+        posts=posts.get_page(1)   
+    except EmptyPage :   
+        posts=posts.get_page(1)   
     context={'posts': posts}
     return render(request,"blog/blog-home.html",context)
 
