afc45afc06134b6e3be462d06dc0a4b966953b19
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-12-07 07:29:02 +0330

    define user login (way2: in template)

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_view(request) :
-    if request.user.is_authenticated :
-        msg = f'user is authenticated as {request.user.username}'
-    else:
-        msg = 'user is not authenticated'    
-    return render(request, 'accounts/login.html',{'msg':msg})
+    return render(request, 'accounts/login.html')
 
 # def logout_view(request) :
 #     return 
