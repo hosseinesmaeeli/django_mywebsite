36821e4921de5c010e1567deaa18aff94ee90000
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-12-07 07:06:03 +0330

    create accounts App

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('website.urls')),
-    path('',include('blog.urls'))
+    path('',include('blog.urls')),
+    path('accounts/',include('accounts.urls'))
     
 
 ]
