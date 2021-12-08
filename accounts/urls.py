94d5a95475fd63c7f2e85dbe76f79d2ac8abec79
Hossein <92952483+hosseinesmaeeli@users.noreply.github.com>
2021-12-08 08:49:48 +0330

    use login requierd decorator
    dont forget "/" after login url in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 app_name='accounts'
 urlpatterns =[
-    path('login',views.login_view,name='login'),
+    path('login/',views.login_view,name='login'),
     #login
     path('logout',views.logout_view,name='logout'),
     #logout
